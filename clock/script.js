adf1fecfa783253a16b27c7edb2451fdf2d5bf7f
scaru727 <31521409+scaru727@users.noreply.github.com>
2018-03-25 18:15:15 -0400

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+setTimeout(function(){
+
+location.reload();
+
+}, 63000);
+
+
+
+
+$(document).ready(function() {
+
+
+
+setTimeout(function(){
+$(".wake-up-text").addClass("alarming");
+//$("body").addClass("alarming");
+
+
+}, 12000);
+
+
+
+$(".button").click(function(){
+
+$(".wake-up-text").removeClass("alarming");	
+
+//$("body").removeClass("alarming");
+
+
+
+
+});
+
+
+
+});
+
+
+
+})
+
